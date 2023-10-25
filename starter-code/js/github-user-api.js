9775239a2b711d6db6760d6b10b1ae7751850496
William Clayton <william.d.clayton1@gmail.com>
2023-10-25 07:59:20 -0500

    -wc

--- starter-code/js/github-user-api.js
+++ starter-code/js/github-user-api.js
@@ -1,5 +1,6 @@
 import GITHUB_API_TOKEN from "./keys.js"
 
+
 //GET request for a list of users' data
 const pullUsersFromApi = async (input) => {
     const url = `https://api.github.com/users/${input}`
@@ -14,11 +15,61 @@ const pullUsersFromApi = async (input) => {
     return await response.json()
 }
 
-// const usernameSearched = async () => {
-//     const search = document.querySelector('#search');
-//     let userInput = search.value;
-//     let gitHubUser = await pullUsersFromApi(userInput);
-//     return gitHubUser.login;
-// }
+//Date Time Conversion
+const dateConversion = (data) => {
+    let date = new Date(data);
+    let dateArray = date.toString().split(' ');
+    let month = dateArray[1];
+    let year = date.getFullYear();
+    let day = date.getDate();
+    return `${day} ${month} ${year}`;
+}
 
-export {pullUsersFromApi}
+
+const usernameSearched = async () => {
+    const search = document.querySelector('#search');
+    const profileTitle = document.querySelector('#profile-title');
+    const profilePicture = document.querySelector('#profile-picture');
+    const profileBio = document.querySelector('#profile-bio');
+    const profileLink = document.querySelector('#profile-username');
+    const profileDate = document.querySelector('#date');
+    let userInput = search.value;
+    let gitHubUser = await pullUsersFromApi(userInput);
+
+    //console logging data
+    console.log(gitHubUser);
+
+    //displaying data on header
+    if (gitHubUser.name != null) {
+        profileTitle.innerHTML = gitHubUser.name;
+    } else {
+        profileTitle.innerHTML = gitHubUser.login;
+    }
+
+    //displaying bio if not null
+    if (gitHubUser.bio != null) {
+        profileBio.innerHTML = gitHubUser.bio;
+        profileBio.style.fontSize = '15px';
+    } else {
+        profileBio.innerHTML = "This profile has no bio";
+    }
+
+    //displaying profile picture
+    profilePicture.src = gitHubUser.avatar_url;
+    profilePicture.style.width = '117px';
+
+    //display @github link
+    profileLink.innerHTML = "@" + gitHubUser.login.toLowerCase();
+
+    //display date
+
+    profileDate.innerHTML = dateConversion(gitHubUser.created_at);
+
+
+
+
+
+
+}
+
+export {pullUsersFromApi, usernameSearched}
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,20 +1,13 @@
-import {pullUsersFromApi} from "./github-user-api.js";
+import {pullUsersFromApi, usernameSearched} from "./github-user-api.js";
 
-const usernameSearched = async () => {
-    const search = document.querySelector('#search');
-    let userInput = search.value;
-    let gitHubUser = await pullUsersFromApi(userInput);
-    return gitHubUser.login;
-}
 
-//Main Method
+// Main Method
 (async () => {
 
     const searchBtn = document.querySelector('#submit-btn');
 
     //on click searches for user data
     searchBtn.addEventListener('click', usernameSearched)
-
 
     //grab the necessary data from the response
     //display it on the DOM
